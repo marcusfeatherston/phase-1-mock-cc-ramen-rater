ae37fdbb511835f363eeb00e47b31040510ac1c0
marcus featherston <marcusovfeatherston@gmail.com>
2023-02-08 11:40:30 -0700

    mock test submit

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
-// See all ramen images in the div with the id of ramen-menu. When the page loads, request the data from the server to get all the ramen objects. Then, display the image for each of the ramen using an img tag inside the #ramen-menu div.
-// Click on an image from the #ramen-menu div and see all the info about that ramen displayed inside the #ramen-detail div and where it says insert comment here and insert rating here.
+const url = "http://localhost:3000/ramens";
 
-// Create a new ramen after submitting the new-ramen form. The new ramen should be added to the#ramen-menu div.
+const ramenMenu = document.querySelector("#ramen-menu");
 
-// The new ramen does not need to persist; in other words, if you refresh the page, it's okay that the new ramen is no longer on the page.
+const imageDetail = document.querySelector(".detail-image");
 
-const URL = "http://localhost:3000";
-const detailMenu = document.getElementById("ramen-menu");
-const detailImage = document.querySelector(".detail-image");
 const ramenName = document.querySelector(".name");
-const detailRestaurant = document.querySelector(".restaurant");
-const detailRating = document.querySelector("#rating-display");
-const detailComment = document.querySelector("#comment-display");
-const ramenForm = document.querySelector("#new-ramen");
 
-const renderTheRamen = (ramen) => {
-  const ramenImg = document.createElement("img");
-  ramenImg.src = ramen.image;
-  detailMenu.append(ramenImg);
+const restaurantName = document.querySelector(".restaurant");
 
-  ramenImg.addEventListener("click", (event) => {
-    detailImage.src = ramen.image;
+const ratingDetail = document.querySelector("#rating-display");
+
+const commentDetail = document.querySelector("#comment-display");
+
+const form = document.querySelector("#new-ramen");
+
+const renderRamen = (ramen) => {
+  const menuImage = document.createElement("img");
+  menuImage.src = ramen.image;
+  ramenMenu.append(menuImage);
+  menuImage.addEventListener("click", (event) => {
+    event.preventDefault();
+    imageDetail.src = ramen.image;
     ramenName.textContent = ramen.name;
-    detailRestaurant.textContent = ramen.restaurant;
-    detailRating.textContent = ramen.rating;
-    detailComment.textContent = ramen.comment;
+    restaurantName.textContent = ramen.restaurant;
+    ratingDetail.textContent = ramen.rating;
+    commentDetail.textContent = ramen.comment;
   });
 };
 
-fetch(URL + "/ramens")
+fetch(url)
   .then((response) => response.json())
-  .then((ramenList) => {
-    ramenList.forEach(renderTheRamen);
+  .then((data) => {
+    console.log(data);
+    data.forEach(renderRamen);
   });
 
-ramenForm.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log(event.target);
   const newRamen = {
-    name: event.target.name.value,
-    restaurant: event.target.restaurant.value,
-    image: event.target.image.value,
-    rating: event.target.rating.value,
     comment: event.target["new-comment"].value,
+    image: event.target.image.value,
+    name: event.target.name.value,
+    rating: event.target.rating.value,
+    restaurant: event.target.restaurant.value,
   };
-  renderTheRamen(newRamen);
+  renderRamen(newRamen);
 });
